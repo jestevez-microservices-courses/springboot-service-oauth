b222ab6f97b558672f5a975c3432c141e7e52ec7
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-01-30 17:06:56 +0100

    New Spring Boot project - OAuth2

---
+++ src/main/java/com/joseluisestevez/msa/oauth/SpringbootServiceOauthApplication.java
@@ -0,0 +1,13 @@
+package com.joseluisestevez.msa.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServiceOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiceOauthApplication.class, args);
+	}
+
+}
