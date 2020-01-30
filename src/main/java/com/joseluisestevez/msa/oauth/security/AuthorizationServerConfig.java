25db01ca84897678f0319439d5f5dd0555909b32
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-01-30 18:13:47 +0100

    Configure clients credentials and tokenKeyAccess

--- src/main/java/com/joseluisestevez/msa/oauth/security/AuthorizationServerConfig.java
+++ src/main/java/com/joseluisestevez/msa/oauth/security/AuthorizationServerConfig.java
@@ -28,15 +28,23 @@ public class AuthorizationServerConfig
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security)
 			throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(security);
+		security.tokenKeyAccess("permitAll()")
+				.checkTokenAccess("isAuthenticated()");
 	}
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients)
 			throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(clients);
+		clients.inMemory().withClient("frontendapp")
+				.secret(passwordEncoder.encode("12345")).scopes("read", "write")
+				.authorizedGrantTypes("password", "refresh_token")
+				.accessTokenValiditySeconds(3600)
+				.refreshTokenValiditySeconds(3600).and()
+				.withClient("androidapp")
+				.secret(passwordEncoder.encode("12345")).scopes("read", "write")
+				.authorizedGrantTypes("password", "refresh_token")
+				.accessTokenValiditySeconds(3600)
+				.refreshTokenValiditySeconds(3600);
 	}
 
 	@Override
